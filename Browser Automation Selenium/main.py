7d43ba46b11d0dc34bed65cb8f788ae3b494aaff
LegitMartin <martin.kubicka2004@gmail.com>
2026-01-30 19:08:49 +0100

    Browser Automation - barebone functioning

--- Browser Automation Selenium/main.py
+++ Browser Automation Selenium/main.py
@@ -1,4 +1,5 @@
 import re
+import os
 
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
@@ -22,8 +23,12 @@ def get_credentials():
 # Define driver, options, service
 chrome_options = Options()
 chrome_options.add_argument("--disable-search-engine-choice-screen")
-service = Service("C:\\Users\\matok\\PycharmProjects\\Python Mega Course\\Browser Automation Selenium\\chromedriver-win64"
-                  "\\chromedriver.exe")
+
+project_path = os.getcwd()
+prefs = {'download.default_directory': project_path}
+chrome_options.add_experimental_option('prefs', prefs)
+
+service = Service(project_path + "\\chromedriver-win64\\chromedriver.exe")
 driver = webdriver.Chrome(service=service, options=chrome_options)
 
 # Load the webpage
@@ -39,6 +44,40 @@ username, password = get_credentials()
 username_field.send_keys(username)
 password_field.send_keys(password)
 driver.execute_script("arguments[0].click();", login_field)
+
+# locate the ELements dropdown and Text box
+elements = WebDriverWait(driver, 10).until(
+    EC.visibility_of_element_located((By.XPATH, '//*[@id="app"]/div/div/div/div[1]/div/div/div[1]/span/div')))
+elements.click()
+
+text_box = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.ID, 'item-0')))
+text_box.click()
+
+# Locate the form fields and submit button
+full_name_field = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.ID, 'userName')))
+email_field = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.ID, 'userEmail')))
+current_address_field = WebDriverWait(driver, 10).until(
+    EC.visibility_of_element_located((By.ID, 'currentAddress')))
+permanent_address_field = WebDriverWait(driver, 10).until(
+    EC.visibility_of_element_located((By.ID, 'permanentAddress')))
+
+submit_field = driver.find_element(By.ID, 'submit')
+
+# Fill in the form fields
+full_name_field.send_keys('John Smith')
+email_field.send_keys('john.smith@gmail.com')
+current_address_field.send_keys('John Street 100, New York, USA')
+permanent_address_field.send_keys('John Street 100, New York, USA')
+
+driver.execute_script('arguments[0].click();', submit_field)
+
+# Locate the Upload and Download button and the Download button
+upload_download = WebDriverWait(driver, 10).until(
+    EC.visibility_of_element_located((By.ID, 'item-7')))
+upload_download.click()
+
+download_button = driver.find_element(By.ID, 'downloadButton')
+driver.execute_script('arguments[0].click();', download_button)
 
 
 input("press enter to close")
