e961150fd457ad20aea1951f655a33c1dbf4eed6
LegitMartin <martin.kubicka2004@gmail.com>
2026-02-02 20:31:20 +0100

    Django From App - Basic frontend done

--- Django Form/job_application/urls.py
+++ Django Form/job_application/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 #4th step (3rd was setting up index.html)
+# Didn't work, had to put this into __init__.py
 
 urlpatterns = [
     path('', views.index, name='index')
--- Django Form/mysite/urls.py
+++ Django Form/mysite/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+# 5th step, second path()
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('job_application'))
 ]
