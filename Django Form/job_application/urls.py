fbc11647061e04b8b8a660b599a7080021d6a657
LegitMartin <martin.kubicka2004@gmail.com>
2026-02-05 16:35:51 +0100

    Django Form App - Added navigation bar, simple admin functionality, About us webpage

--- Django Form/job_application/urls.py
+++ Django Form/job_application/urls.py
@@ -5,5 +5,6 @@ from . import views
 # Didn't work, had to put this into __init__.py
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('about/', views.about, name='about')
 ]
--- Django Form/mysite/settings.py
+++ Django Form/mysite/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -117,6 +119,8 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+ENV_FILE = BASE_DIR / '.env'
+
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
 EMAIL_HOST = 'smtp.gmail.com'
@@ -125,6 +129,8 @@ EMAIL_PORT = 587
 
 EMAIL_USE_TLS = True
 
-EMAIL_HOST_USER = 'mato.kubicka@gmail.com'
+load_dotenv(ENV_FILE)
 
-EMAIL_HOST_PASSWORD = 'aecrwjzysbiodxuf'
+EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
+
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
